6eb4371a818fd7f6c7f36b29edee87aec53f7494
Moona <moonapark1126@gmail.com>
2025-07-04 10:05:34 +0900

    feat:modify calculator3

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,8 @@ public class Calculator  {
     public int add(String str1, String str2) {
         int a =Integer.parseInt(str1);
         int b =Integer.parseInt(str2);
+        long longA =a;
+        long longB =b;
         return a + b;
     }
 }
